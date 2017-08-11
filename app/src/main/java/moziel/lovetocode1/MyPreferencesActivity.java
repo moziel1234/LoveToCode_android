cda701030267801dd2a3334f70ed608f1be396c4
Moshe Oziel <moziel@cisco.com>
2017-08-11 11:10:45 +0300

    Pref with value on summary

--- app/src/main/java/moziel/lovetocode1/MyPreferencesActivity.java
+++ app/src/main/java/moziel/lovetocode1/MyPreferencesActivity.java
@@ -1,8 +1,13 @@
 package moziel.lovetocode1;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.EditTextPreference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceScreen;
+
+import java.util.Map;
 
 public class MyPreferencesActivity extends PreferenceActivity {
 
@@ -12,13 +17,50 @@ public class MyPreferencesActivity extends PreferenceActivity {
         getFragmentManager().beginTransaction().replace(android.R.id.content, new MyPreferenceFragment()).commit();
     }
 
-    public static class MyPreferenceFragment extends PreferenceFragment
-    {
+    public static class MyPreferenceFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener {
+        SharedPreferences sharedPreferences;
+
         @Override
-        public void onCreate(final Bundle savedInstanceState)
-        {
+        public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
+
+            // load the preferences from your XML resource (which I assume you already do anyway)
             addPreferencesFromResource(R.xml.preferences);
+        }
+
+        @Override
+        public void onResume() {
+            super.onResume();
+
+            sharedPreferences = getPreferenceManager().getSharedPreferences();
+
+            // we want to watch the preference values' changes
+            sharedPreferences.registerOnSharedPreferenceChangeListener(this);
+
+            updateAllPrefs();
+        }
+
+        @Override
+        public void onPause() {
+            sharedPreferences.unregisterOnSharedPreferenceChangeListener(this);
+            super.onPause();
+        }
+
+        @Override
+        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+            updateAllPrefs();
+        }
+
+        private void updateAllPrefs() {
+            PreferenceScreen preferenceScreen = getPreferenceScreen();
+            for(int i = 0; i < preferenceScreen.getPreferenceCount(); i++) {
+                updateSummary((EditTextPreference) getPreferenceScreen().getPreference(i));
+            }
+        }
+
+        private void updateSummary(EditTextPreference preference) {
+            // set the EditTextPreference's summary value to its current text
+            preference.setSummary(preference.getText());
         }
     }
 
