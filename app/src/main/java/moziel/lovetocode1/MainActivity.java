1f89095a27d67a7c17228cf31b2041e16c15b6c8
Moshe Oziel <moziel@cisco.com>
2017-08-12 22:08:51 +0300

    add target

--- app/src/main/java/moziel/lovetocode1/MainActivity.java
+++ app/src/main/java/moziel/lovetocode1/MainActivity.java
@@ -1,7 +1,10 @@
 package moziel.lovetocode1;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -9,6 +12,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +22,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        TextView target = (TextView)findViewById(R.id.text_target);
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        SharedPreferences.OnSharedPreferenceChangeListener  listener = new SharedPreferences.OnSharedPreferenceChangeListener() {
+            public void onSharedPreferenceChanged(SharedPreferences prefs, String key) {
+                if (key.equals("pref_num_target")) {
+                    TextView target = (TextView)findViewById(R.id.text_target);
+                    target.setText(prefs.getString("pref_num_target", "0"));
+                }
+            }
+        };
+
+        preferences.registerOnSharedPreferenceChangeListener(listener);
+
+
+        target.setText(preferences.getString("pref_num_target", "0"));
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
