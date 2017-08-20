7c2b6cf1287961ee04adf9fa86572166527ad93c
Moshe Oziel <moziel@cisco.com>
2017-08-20 10:47:05 +0300

    Add some unitests and fix last bug

--- app/src/main/java/moziel/lovetocode1/Calc.java
+++ app/src/main/java/moziel/lovetocode1/Calc.java
@@ -20,6 +20,7 @@ public class Calc {
         int total = (int) Math.pow(n, k);
 
         while (total-- > 0) {
+            // System.out.println( "----------------------------------");
             for (int i = 1; i < eqArray.length; i++) {
                 if (i == 1)
                     left = eqArray[i-1];
@@ -36,7 +37,7 @@ public class Calc {
                 }  else if("/".equals(currOpt)) {
                     res = left / right;
                 }
-                //Log.d("Mosh:", String.format("left = %d, opt = %s, right = %d, res = %d ", left, currOpt, right, res));
+                // System.out.println( String.format("left = %.2f, opt = %s, right = %.2f, res = %.2f ", left, currOpt, right, res));
             }
             if (res==target) {
                 String stRes = "";
@@ -46,7 +47,7 @@ public class Calc {
                 return stRes;
             }
 
-            for (int i = 0; i < n; i++) {
+            for (int i = 0; i < k; i++) {
                 if (indexes[i] >= n - 1) {
                     indexes[i] = 0;
                 } else {
--- app/src/test/java/moziel/lovetocode1/ExampleUnitTest.java
+++ app/src/test/java/moziel/lovetocode1/ExampleUnitTest.java
@@ -8,8 +8,42 @@ import static org.junit.Assert.*;
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
 public class ExampleUnitTest {
+    char[] chars = {'+', '-', '*', '/'};
     @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+    public void a1() throws Exception {
+        int[] eqArray = {1,2,3};
+        assertEquals(Calc.permute(chars, eqArray, 120), "");
     }
+    @Test
+    public void a2() throws Exception {
+        int[] eqArray = {2,3,4,5};
+        assertEquals(Calc.permute(chars, eqArray, 120), "***");
+    }
+    @Test
+    public void a3() throws Exception {
+        int[] eqArray = {2,3,4,5,6,6};
+        assertEquals(Calc.permute(chars, eqArray, 120), "***-+");
+    }
+    @Test
+    public void a4() throws Exception {
+        int[] eqArray = {2,3,4,5,6,6,8};
+        String temp = Calc.permute(chars, eqArray, 120);
+        assertEquals(temp.length(), eqArray.length-1);
+        assertEquals(temp, "+**+++");
+    }
+    @Test
+    public void a5() throws Exception {
+        int[] eqArray = {2,3,4,5,6,6,8,8};
+        String temp = Calc.permute(chars, eqArray, 120);
+        assertEquals(temp.length(), eqArray.length-1);
+        assertEquals(temp, "***-+-+");
+    }
+    @Test
+    public void a6() throws Exception {
+        int[] eqArray = {2,3,4,5,7};
+        String temp = Calc.permute(chars, eqArray, 120);
+        assertEquals(temp.length(), 0);
+        assertEquals(temp, "");
+    }
+
 }
